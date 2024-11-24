87a3a406c78cc4adc351d59a44b319d09e5c1e5d
ElderGr <gbielrs@gmail.com>
2024-11-24 10:58:16 -0300

    feat: add custom orm and apply it on Transaction Entity

---
+++ backend/ride/src/application/usecases/GetTransactionByRideId.ts
@@ -0,0 +1,27 @@
+import { Transaction } from "../../domain/Transaction";
+import { TransactionRepository } from "../repository/TransactionRepository";
+
+export class GetTransactionByRideId {
+    constructor(
+        private transactionRepository: TransactionRepository
+    ){}
+
+    async execute(rideId: string): Promise<Output>{
+        const transaction = await this.transactionRepository.getByRideId(rideId);
+        return {
+            transactionId: transaction.transactionId,
+            rideId: transaction.rideId,
+            amount: transaction.amount,
+            date: transaction.date,
+            status: transaction.getStatus()
+        };
+    }
+}
+
+type Output = {
+    transactionId: string,
+    rideId: string,
+    amount: number,
+    date: Date,
+    status: string
+}
